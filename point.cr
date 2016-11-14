83136d2fb6ffd2df1cc92678214c53e76e2fe311
Leon Rische <leon.rische@student.uni-siegen.de>
2016-11-14 02:47:44 +0100

    Initial commit

---
+++ point.cr
@@ -0,0 +1,25 @@
+module LA
+  struct Point1(T)
+    getter x : T
+
+    def initialize(@x)
+    end
+  end
+
+  struct Point2(T)
+    getter x : T
+    getter y : T
+
+    def initialize(@x, @y)
+    end
+  end
+
+  struct Point3(T)
+    getter x : T
+    getter y : T
+    getter z : T
+
+    def initialize(@x, @y, @z)
+    end
+  end
+end
