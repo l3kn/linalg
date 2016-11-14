7f168c2e768e89e26c97d2463ebe5d9fda1a57f9
Leon Rische <leon.rische@student.uni-siegen.de>
2016-11-14 02:55:24 +0100

    Extract macros to second file

---
+++ src/vector_macros.cr
@@ -0,0 +1,46 @@
+macro define_vector_op(op, components)
+  def {{op.id}}(other : self)
+    self.new(
+      {% for c in components %}
+      {{c.id}} {{op.id}} other.{{c.id}},
+      {% end %}
+    )
+  end
+end
+
+macro define_op(op, components, type)
+  def {{op.id}}(other : {{type.id}})
+    self.new(
+      {% for c in components %}
+      {{c.id}} {{op.id}} other,
+      {% end %}
+    )
+  end
+end
+
+macro define_dot(components)
+  def dot(other : self)
+    {% for c in components[0..-1] %}
+      {{c.id}} * other.{{c.id}} + 
+    {% end %}
+    {{components[-1].id}} * other.{{components[-1].id}} 
+  end
+end
+
+macro define_length(components)
+  def length
+    Math.sqrt({% for c in components[0..-1] %} c * {% end %} {{components[-1].id}})
+  end
+
+  def squared_length
+    {% for c in components[0..-1] %} c * {% end %} {{components[-1].id}}
+  end
+
+  def normalize
+    {% if components.size == 1 %}
+      self / length
+    {% else %}
+      self * (1.0 / length)
+    {% end %}
+  end
+end
